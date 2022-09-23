39c17322a7cc51f4179f6c145cc337b8bef03ef5
Elena <volshebstvuyu@gmail.com>
2022-09-24 00:04:27 +0300

    fix def with search

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
-from  exception import  ElementNotInList
+from exception import ElementNotInList
+
 
 def binary_search(array: [], x: int, lo=0, hi=None):
     if hi is None:
         hi = len(array)
+
     while lo < hi:
-        mid = (lo+hi)//2
-        midval = array[mid]
-        if midval < x:
-            lo = mid+1
-        elif midval > x:
+        mid = (lo + hi) // 2
+        midVal = array[mid]
+        if midVal < x:
+            lo = mid + 1
+        elif midVal > x:
             hi = mid
         else:
-            return mid
+            return mid - 1
+    raise ElementNotInList('Элемент не входит в список', array.index(mid - 1))
 
-
-    raise ElementNotInList('Элемент не входит в список', array[mid])
 
 def tryParseString(string):
     try:
@@ -22,8 +23,9 @@ def tryParseString(string):
         _array = list(map(int, _array))
         _array.sort()
         return _array
-    except Exception as e:
+    except:
         return 'Не удалось распарсить строку'
+
 
 inputString = '1 25 2 12 4 9 26 17 5 7 11'
 inputInt = int(input('введите любое число, только целое =) '))
@@ -38,10 +40,11 @@ if type(arr) != str:
     except ElementNotInList as e:
         resultMsg, resultIndex = e()
 
-if resultMsg != None:
+
+if resultMsg is not None:
     print(resultMsg)
     print(resultIndex)
-elif result != None:
+elif result is not None:
     print(result)
 
 print(arr)
